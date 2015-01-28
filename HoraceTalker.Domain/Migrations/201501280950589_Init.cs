9bfabf4a8097015aba5c37ae14e833d4999c8501
Richard Granville <richard.granville@just-eat.com>
2015-01-28 10:36:02 +0000

    Added initial DB migration

---
+++ HoraceTalker.Domain/Migrations/201501280950589_Init.cs
@@ -0,0 +1,27 @@
+namespace HoraceTalker.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(nullable: false, maxLength: 50),
+                        Password = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
